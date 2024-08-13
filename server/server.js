d59b9c97e4d54afb956efc9d505ef84ace360326
juliette maloke <your@email.com>
2024-08-12 20:51:33 -0500

    Updated server.js to use dotenv for environment variables and corrected server configuration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,55 @@
 
+// require('dotenv').config();
+
+// const express = require('express');
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const startApolloServer = async () => {
+//   await server.start();
+
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(express.json());
+
+//   // Apply Apollo middleware with authentication context
+//   app.use('/graphql', expressMiddleware(server, {
+//     context: async ({ req }) => {
+//       return authMiddleware(req);
+//     }
+//   }));
+
+//   // Serve static files in production
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//     });
+//   }
+
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// startApolloServer();
+
+
 require('dotenv').config();
 
 const express = require('express');
@@ -11,6 +62,9 @@ const db = require('./config/connection');
 
 
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.SECRET;
+
 const app = express();
 
 const server = new ApolloServer({
@@ -39,6 +93,7 @@ const startApolloServer = async () => {
     });
   }
 
+  // Connect to the database and start the server
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
