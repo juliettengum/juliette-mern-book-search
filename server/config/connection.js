271ccfb6f451fc7a85af69038540f0ec6ceb9d20
juliette maloke <your@email.com>
2024-08-12 21:20:52 -0500

    Update MongoDB connection configuration in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,7 @@
 // module.exports = mongoose.connection;
 
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearch');
   
 
